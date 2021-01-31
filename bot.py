122127dbf82373bed4bca7f09900d0f62ab120d5
navacar <55153165+navacar@users.noreply.github.com>
2021-01-31 21:53:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,15 @@ import requests
 import telebot
 import instaloader
 from bs4 import BeautifulSoup
+<<<<<<< Updated upstream
 
 from config import TELEGRAM_TOKEN, INST_USERNAME_BOT, INST_PASSWORD_BOT, START_TEXT
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+=======
+import os
+bot = telebot.TeleBot('1669911641:AAFcOx45b8c4ULDzo43ISLn8WfV4y7RAPKw')
+>>>>>>> Stashed changes
 
 oneTime = False
 start = False
@@ -30,19 +35,43 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    response = requests.get("https://instagram.com/" + message.text + "/")
-    if response.status_code != 404:
+    
+    if profileCheck(message.text):
         bot.send_message(message.chat.id, "Ждите, считаем подписчиков")
         subscribers_list(message.text)
     else:
         bot.send_message(message.chat.id, "Вы ввели неправильное имя аккаунта")
 
 
+<<<<<<< Updated upstream
 def subscribers_list(username):
     profile = instaloader.Profile.from_username(loader.context, username)
+=======
+def subscribersList(username):
+    profile = instaloader.Profile.from_username(L.context, username)
+    subList = []
+    
+>>>>>>> Stashed changes
     for followee in profile.get_followers():
-        subscribers = followee.username
-        print(subscribers)
+        subscriber = followee.username
+        subList.append(subscriber)
+
+    return subList
+
+# def profileCheck(username):
+#     response = requests.get("https://instagram.com/" + username + "/")
+#     isExist = True
+
+#     with open('test.html', 'w') as output_file:
+#         output_file.write(response.text)
+#     word = "Page Not Found"
+    
+#     with open('test.html', 'r') as file:
+#         for line in file:
+#             if word in line:
+#                 print("Da")
+#                 isExist = False
+#     return isExist
 
 
 bot.polling()
