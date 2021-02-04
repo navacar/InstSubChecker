57b65b3ac716af41cbeec4d68e7a94e2412891eb
navacar <55153165+navacar@users.noreply.github.com>
2021-02-04 22:21:40 +0200

    Main function
    Сделал основную функцию, и чек на существования профиля

--- bot.py
+++ bot.py
@@ -1,20 +1,25 @@
-import requests
 import telebot
 import instaloader
-from bs4 import BeautifulSoup
-import os
+import requests
 
-from config import TELEGRAM_TOKEN, INST_USERNAME_BOT, INST_PASSWORD_BOT, START_TEXT, ERROR_MESSAGE
 from Log import Log
 from DataBase import DataBase
 
+from config import TELEGRAM_TOKEN, INST_USERNAME_BOT, INST_PASSWORD_BOT, START_TEXT, ERROR_MESSAGE
+
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
 logAdapter = Log()
 dbAdapter = DataBase(main_log=logAdapter)
 
+start = False
+
+profile = instaloader.Instaloader()
 loader = instaloader.Instaloader()
 loader.login(INST_USERNAME_BOT, INST_PASSWORD_BOT)
 
+def list2str(arr):
+    return '\n'.join(arr) + '\n'
 
 def add_user(chat_id, inst_login):
     """
@@ -60,21 +65,21 @@ def update_followers(chat_id, inst_login):
     return status is not None
 
 
-def list2str(arr):
-    return '\n'.join(arr) + '\n'
-
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    global start
+
     if message.text == '/start':
         bot.send_message(message.chat.id, START_TEXT)
+        start = True
     else:
-        inst = message.text.split()[1]
-        status = add_user(message.chat.id, inst)
-        if isinstance(status, str):
-            bot.send_message(message.chat.id, status)
-        else:
-            bot.send_message(message.chat.id, ERROR_MESSAGE if not status else 'Я тебя запомнил;)')
+        bot.send_message(message.chat.id, "Пришли имя аккаунта")
+        # inst = message.text.split()[1]
+        # status = add_user(message.chat.id, inst)
+        # if isinstance(status, str):
+        #     bot.send_message(message.chat.id, status)
+        # else:
+        #     bot.send_message(message.chat.id, ERROR_MESSAGE if not status else 'Я тебя запомнил;)')
 
 
 @bot.message_handler(commands=['unsub'])
@@ -126,45 +131,44 @@ def delete_account_command(message):
     status = dbAdapter.delete_user(message.chat.id, inst)
     if not status:
         bot.send_message(message.chat.id, ERROR_MESSAGE)
+    global start
+    start = False
+
+    if not False:
+        bot.send_message(message.chat.id, START_TEXT)
+        start = True
     else:
         bot.send_message(message.chat.id, 'Аккаунт {} больше не отслеживается'.format(inst))
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    # По факту send_text не нужен, я бы удалил или добавил подсказку.
-    if profileCheck(message.text):
+    global start
+
+    if profileCheck(message.text) and start:
         bot.send_message(message.chat.id, "Ждите, считаем подписчиков")
-        # subscribersList(message.text)
-    else:
+        subList = subscribersList(message.text)
+        # Это заглушка чтобы понимать что функция сработала до конца
+        bot.send_message(message.chat.id, "У вас " + str(len(subList)) + " подписчиков")
+    elif start:
         bot.send_message(message.chat.id, "Вы ввели неправильное имя аккаунта")
 
-
 def subscribersList(username):
-    profile = instaloader.Profile.from_username(loader.context, username)
     subList = []
-    
     for followee in profile.get_followers():
-        subscriber = followee.username
-        subList.append(subscriber)
-
+        subList.append(followee.username)
     return subList
 
 
 def profileCheck(username):
-    # response = requests.get("https://instagram.com/" + username + "/")
+    global profile
     isExist = True
 
-    # with open('test.html', 'w') as output_file:
-    #     output_file.write(response.text)
-    # word = "Page Not Found"
-    
-    # with open('test.html', 'r') as file:
-    #     for line in file:
-    #         if word in line:
-    #             print("Da")
-    #             isExist = False
-    return isExist
+    try: 
+        profile = instaloader.Profile.from_username(loader.context, username) 
+    except Exception as e:  
+        isExist = False
 
+    return isExist
 
 bot.polling()
